dd570be8027db74ab21691d6df5b46687e33543d
Adrian Gabriel <lycanboss1995@gmail.com>
2021-09-29 11:12:13 -0300

    Adicionar arquivos de projeto.

---
+++ ficha programação5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ficha_programação5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float numero1, numero2, numero3;
+            Console.WriteLine("Informe seu ano de nascimento:");
+            numero1 = float.Parse(Console.ReadLine());
+            numero2 = 2021 - numero1;
+            numero3 = 2022 - numero1;
+            Console.WriteLine("Sua idade atual é:{0} anos", numero2);
+            Console.WriteLine("Em 2022 voçê terá:{0} anos", numero3);
+            Console.ReadLine();
+        }
+    }
+}
